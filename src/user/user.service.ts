c6dbc8913b6140b2a764f2f18e701737c63ff340
o1key222k <o1key222k@gmail.com>
2026-03-29 12:23:40 +0700

    feat: implement user CRUD operations and pagination with GraphQL resolver and service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from 'src/generated/prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUserDto, UserFilter } from './dto/user.dto';
+import { UserPaginationResponse } from './models/user.model';
 
 @Injectable()
 export class UserService {
@@ -12,8 +14,54 @@ export class UserService {
     });
   }
 
-  async findAll(): Promise<User[]> {
-    return await this.prisma.user.findMany();
+  async findAll(filter: UserFilter): Promise<UserPaginationResponse> {
+    const search = filter.search || '';
+    const itemsPerPage = Number(filter.itemsPerPage) || 10;
+    const page = Number(filter.page) || 1;
+
+    console.log({ search });
+
+    const skip = page > 1 ? (page - 1) * itemsPerPage : 0;
+    const users = await this.prisma.user.findMany({
+      take: itemsPerPage,
+      skip,
+      where: {
+        OR: [
+          {
+            username: {
+              contains: search,
+            },
+          },
+          {
+            email: {
+              contains: search,
+            },
+          },
+        ],
+      },
+    });
+
+    const total = await this.prisma.user.count({
+      where: {
+        OR: [
+          {
+            username: {
+              contains: search,
+            },
+            email: {
+              contains: search,
+            },
+          },
+        ],
+      },
+    });
+
+    return {
+      data: users,
+      total,
+      currentPage: page,
+      itemsPerPage,
+    };
   }
 
   async findOne(id: number): Promise<User> {
@@ -26,5 +74,48 @@ export class UserService {
       throw new Error('User not found');
     }
     return user;
+  }
+
+  async update(id: number, dataUpdate: UpdateUserDto): Promise<User> {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+
+    return await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: dataUpdate,
+    });
+  }
+
+  async delete(id: number): Promise<boolean> {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        id,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+
+    try {
+      await this.prisma.user.delete({
+        where: {
+          id,
+        },
+      });
+
+      return true;
+    } catch (error) {
+      console.log('error=>', error);
+      return false;
+    }
   }
 }
