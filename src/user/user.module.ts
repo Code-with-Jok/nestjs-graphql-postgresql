28607e1d4604aeeb367673803bacb41be9e0c884
o1key222k <o1key222k@gmail.com>
2026-03-29 14:53:22 +0700

    feat: implement authentication module with JWT guard and user management services

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [UserResolver, UserService],
+  providers: [UserResolver, UserService, JwtService, PrismaService],
 })
 export class UserModule {}
